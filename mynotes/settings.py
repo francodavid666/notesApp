d21c805cdd252a900f2a904fbe3149611e17e808
franco66 <david_franco@outlook.com>
2023-09-25 14:44:41 -0300

    g

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = 'mynotes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'frontend','build')],
+        "DIRS": [os.path.join(BASE_DIR, "frontend", "build")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
